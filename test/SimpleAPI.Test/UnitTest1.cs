f0270583cbb2abd8fe85026849abecf035296c94
Valera Nikitin <nikitinvalera@gmail.com>
2020-04-02 21:02:29 -0400

    initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using SimpleAPI.Controllers;
+using System;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller;
+
+        public UnitTest1()
+        {
+            controller = new WeatherForecastController();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var expected = controller.Get(10);
+
+            Assert.Equal("Val", expected.Value);
+        }
+    }
+}
